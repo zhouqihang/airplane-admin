6bd30583ab4b89d25150cb59c9e9a72c65bdac39
qihang <qihang_zhou@qq.com>
2022-11-02 19:01:16 +0800

    ✨ 添加样式计算hook

---
+++ src/hooks/useCssEditor.ts
@@ -0,0 +1,84 @@
+import React, { useState, CSSProperties, cloneElement, useEffect } from "react";
+
+interface ICustomState {
+  // hasBorder: boolean;
+}
+
+type stateType = ICustomState & CSSProperties;
+
+interface IBindVaueParams {
+  event: string;
+  field: keyof stateType;
+}
+
+export default function useCssEditor(defaultValue?: stateType) {
+  const [cssState, setCssState] = useState<stateType>({
+    padding: '',
+    margin: '',
+    backgroundColor: '',
+    borderRadius: '',
+    border: '',
+    ...defaultValue
+  });
+  const keys: Array<keyof CSSProperties> = ['padding', 'margin', 'backgroundColor', 'borderRadius', 'border'];
+  const [style, setStyle] = useState<CSSProperties>({})
+
+  useEffect(function () {
+    setStyle(getStyleObject());
+  }, [cssState]);
+
+  function getStyleObject() {
+    const computedStyle: CSSProperties = {};
+    keys.forEach(k => {
+      assignValid<CSSProperties>(computedStyle, k, cssState[k]);
+    })
+
+    // border
+    // if (cssState.hasBorder) {
+    //   style.border = `${cssState.borderWidth}px ${cssState.borderStyle} ${cssState.borderColor}`;
+    // }
+    return computedStyle;
+  }
+
+  function generateSetFunc(key: keyof stateType) {
+    return function (val: unknown) {
+      setCssState({
+        ...cssState,
+        [key]: val
+      })
+    }
+  }
+
+  function bindValue(node: React.ReactElement) {
+    return function (options: IBindVaueParams) {
+      let onChangeEvent = null;
+      if (options.event === 'onChange') {
+        onChangeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+          setCssState(Object.assign({}, cssState, options.field ? {
+            [options.field]: event.target.value
+          } : event.target.value))
+        }
+      }
+      if (options.event === 'onChangeWithValue') {
+        onChangeEvent = (value: unknown) => {
+          console.log("🚀 ~ file: useCssEditor.ts ~ line 70 ~ value", value)
+          setCssState(Object.assign({}, cssState, options.field ? {
+            [options.field]: value
+          } : value))
+        }
+      }
+      return cloneElement(node, {
+        onChange: onChangeEvent,
+        value: cssState[options.field]
+      })
+    }
+  }
+
+  return { cssState, style, generateSetFunc, getStyleObject, bindValue };
+}
+
+function assignValid<T>(obj: T, key: keyof T, value: any) {
+  if (value) {
+    obj[key] = value;
+  }
+}
